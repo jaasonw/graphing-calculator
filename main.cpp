4f6c61c0eddaea0922f6da45d1804e5fc74db228
jaasonw <jayywong92@gmail.com>
2018-11-28 08:58:32 -0800

    clean comments

--- main.cpp
+++ main.cpp
@@ -3,33 +3,5 @@
 
 int main() {
     GraphingCalc g;
-
-    // std::vector<Entity*> entities = std::vector<Entity*>();
-    // sf::RenderWindow window(sf::VideoMode(SCREEN_HEIGHT, SCREEN_WIDTH), "hihi");
-    // window.setFramerateLimit(FPS);
-
-    // entities.push_back(new Graph());
-
-    // while (window.isOpen()) {
-    //     sf::Event event;
-    //     while (window.pollEvent(event)) {
-    //         if (event.type == sf::Event::Closed)
-    //             window.close();
-    //     }
-
-    //     // step loop
-    //     for (int i = 0; i < entities.size(); i++) {
-    //         entities.at(i)->step();
-    //     }
-
-    //     window.clear();
-
-    //     // render loop
-    //     for (int i = 0; i < entities.size(); i++) {
-    //         entities.at(i)->render(window);
-    //     }
-
-    //     window.display();
-    // }
     return 0;
 }
