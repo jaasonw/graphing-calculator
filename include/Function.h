6a64edcfafd1b54e77cfcf87a4994763ea695bb2
jaasonw <jayywong92@gmail.com>
2018-11-29 12:09:50 -0800

    begin working on point

--- include/Function.h
+++ include/Function.h
@@ -4,12 +4,15 @@
 
 class Function : public Entity {
 private:
-    /* data */
+    Queue<Token*> postfix;
 public:
     Function(/* args */);
     ~Function();
+    void step();
+    void render(sf::RenderWindow& window);
 };
 
 Function::Function(/* args */) {}
-
 Function::~Function() {}
+void Function::step() {}
+void Function::render(sf::RenderWindow& window) {}
--- include/Point.h
+++ include/Point.h
@@ -1,1 +1,48 @@
 #pragma once
+#include "engine/Entity.h"
+
+class Point : public Entity {
+private:
+    double graph_x;
+    double graph_y;
+    const double radius = 2;
+    sf::Color color;
+    sf::CircleShape point;
+
+public:
+    Point();
+    Point(double graph_x, double graph_y);
+    ~Point();
+    void step();
+    void render(sf::RenderWindow& window);
+
+    void plot(double screen_x, double screen_y);
+    void setColor(sf::Color color) { this->color = color; }
+    
+    double get_graph_x() { return graph_x; }
+    double get_graph_y() { return graph_y; }
+
+    void set_graph_x(double graph_x) { this->graph_x = graph_x; }
+    void set_graph_y(double graph_y) { this->graph_y = graph_y; }
+};
+
+Point::Point() {
+    this->point.setRadius(radius);
+    this->point.setFillColor(color);
+}
+
+Point::Point(double graph_x, double graph_y)
+    : graph_x(graph_x), graph_y(graph_y) {
+    this->point.setRadius(radius);
+    this->point.setFillColor(color);
+}
+Point::~Point() {}
+void Point::step() {}
+
+void Point::render(sf::RenderWindow& window) {
+    window.draw(this->point);
+}
+
+// void Point::plot(double screen_x, double screen_y) {
+//     this->point.setPosition(screen_x, screen_y);
+// }
