9370f59a9a7e972375e87a04859c5eb3f7230806
jaasonw <jayywong92@gmail.com>
2018-12-02 16:38:38 -0800

    slightly less scuffed graph

--- src/Function.cpp
+++ src/Function.cpp
@@ -3,11 +3,20 @@
 
 Function::Function() : Entity() {
     points.push_back(new Point(0, 0));
-    points.push_back(new Point(0, 1));
-    points.push_back(new Point(0, 2));
-    points.push_back(new Point(0, 3));
-    points.push_back(new Point(0, 4));
-    points.push_back(new Point(0, 5));
+    points.push_back(new Point(1, 1));
+    points.push_back(new Point(2, 2));
+    points.push_back(new Point(3, 3));
+    points.push_back(new Point(4, 4));
+    points.push_back(new Point(5, 5));
+    points.push_back(new Point(6, 6));
+    points.push_back(new Point(7, 7));
+    points.push_back(new Point(8, 8));
+    points.push_back(new Point(9, 9));
+    points.push_back(new Point(10, 10));
+    points.push_back(new Point(11, 11));
+    points.push_back(new Point(12, 12));
+    points.push_back(new Point(13, 14));
+    points.push_back(new Point(15, 15));
 }
 Function::~Function() {
     for (auto point : points) {
