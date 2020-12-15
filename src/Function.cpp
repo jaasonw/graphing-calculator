5bf29976d4e18905b1d5970bf9aa60af516a345d
jaasonw <jayywong92@gmail.com>
2020-12-15 15:09:19 -0800

    only render on screen

--- src/Function.cpp
+++ src/Function.cpp
@@ -1,32 +1,33 @@
 #include "Function.h"
+
 #include "engine/Engine.h"
 
 Function::Function() : Entity() {
     // example test function
-    this->function_string = "sin(x)";
-    this->postfix = infix_to_postfix(tokenize(function_string));
+    function_string = "sin(x)";
+    postfix = infix_to_postfix(tokenize(function_string));
     // for (auto it = postfix.b; it != postfix.back(); it++) {
     //     std::cout << **it << std::endl;
     // }
-    this->update_graph();
+    update_graph();
 
-    this->color.r = rand() % 255;
-    this->color.g = rand() % 255;
-    this->color.b = rand() % 255;
+    color.r = rand() % 255;
+    color.g = rand() % 255;
+    color.b = rand() % 255;
 }
 
-Function::Function(std::string function_string) : Entity(), function_string(function_string) {
-    this->color.r = rand() % 255;
-    this->color.g = rand() % 255;
-    this->color.b = rand() % 255;
+Function::Function(std::string function_string)
+    : Entity(), function_string(function_string) {
+    color.r = rand() % 255;
+    color.g = rand() % 255;
+    color.b = rand() % 255;
     try {
-        this->postfix = infix_to_postfix(tokenize(function_string));
-    }
-    catch(...) {
+        postfix = infix_to_postfix(tokenize(function_string));
+    } catch (...) {
         std::cout << "error parsing function: " << function_string << std::endl;
         return;
     }
-    this->update_graph();
+    update_graph();
 }
 
 // Function::~Function() {
@@ -43,12 +44,17 @@ void Function::step(sf::RenderWindow& window, sf::Event& event, bool poll) {
 void Function::render(sf::RenderWindow& window) {
     // do translating to screen coords here
     for (unsigned int i = 0; i < points.size(); i++) {
-        double x1 = (points[i].x * this->zoom) + origin_x;
-        double y1 = -((points[i].y * this->zoom)) + origin_y;
         if (i != 0) {
-            double x2 = (points[i - 1].x * this->zoom) + origin_x;
-            double y2 = -((points[i - 1].y * this->zoom)) + origin_y;
-            window.draw(create_line(x1, y1, x2, y2, this->color));
+            double x1 = (points[i].x * zoom) + origin_x;
+            double y1 = -((points[i].y * zoom)) + origin_y;
+            double x2 = (points[i - 1].x * zoom) + origin_x;
+            double y2 = -((points[i - 1].y * zoom)) + origin_y;
+            if ((x1 <= window.getSize().x + window.getSize().x / 4 && x1 >= 0 &&
+                y1 <= window.getSize().y + window.getSize().y / 4 && y1 >= 0) ||
+                (x2 <= window.getSize().x + window.getSize().x / 4 && x2 >= 0 &&
+                y2 <= window.getSize().y + window.getSize().y / 4 && y2 >= 0)) {
+                window.draw(create_line(x1, y1, x2, y2, color));
+            }
         }
     }
 }
@@ -63,21 +69,21 @@ void Function::update_graph() {
     for (double i = interval_start; i < interval_end; i += point_frequency) {
         try {
             points.push_back(sf::Vector2f(i, eval(postfix, i)));
-        }
-        catch(...) {
-            std::cout << "something went wrong here, maybe an asymtote, maybe an invalid expression" << std::endl;
+        } catch (...) {
+            std::cout << "something went wrong here, maybe an asymtote, maybe "
+                         "an invalid expression"
+                      << std::endl;
         }
     }
 }
 
 void Function::set_bounds(double low, double high) {
     if (low > high) {
-        this->interval_start = high;
-        this->interval_end = low;
+        interval_start = high;
+        interval_end = low;
+    } else {
+        interval_start = low;
+        interval_end = high;
     }
-    else {
-        this->interval_start = low;
-        this->interval_end = high;
-    }
-    this->update_graph();
+    update_graph();
 }
